1a83d4ae266d0c5765ea01188009c42befc0cb6e
ry-zakari <rufaig6@gmail.com>
2025-07-01 22:10:26 +0000

    Deploying to gh-pages from @ ry-zakari/ry-zakari.github.io@aba72e1dddbf0ba7adf3a7c123d43f75fa05729c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -439,7 +439,7 @@ ninja.data = [{
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
-          description: "with background image",
+          description: "An Enhanced Wildfire Spread Prediction Using Multimodal Satellite Imagery and Deep Learning Models",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
