533d687b9757730f9fb4f9b870ffa869765062f3
ry-zakari <rufaig6@gmail.com>
2025-07-02 16:39:30 +0000

    Deploying to gh-pages from @ ry-zakari/ry-zakari.github.io@9fa31bcc5d647c0e1aa37f98fd8faa4c4fbf7cc9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -447,8 +447,8 @@ ninja.data = [{
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
+            },},{id: "projects-project-3",
+          title: 'project 3',
           description: "A Physics-Informed Neural Network for Next-Day Wildfire Spread Prediction Using U-net and Remote Sensing Data",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
