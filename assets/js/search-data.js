8a76ce48c5c0b3f56ea54384c3fab760c47f9fe3
ry-zakari <rufaig6@gmail.com>
2025-07-04 18:30:42 +0000

    Deploying to gh-pages from @ ry-zakari/ry-zakari.github.io@ad460c2aeaaf86dab33ab1fd4821e4d3c157a1e7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,14 +17,14 @@ ninja.data = [{
             window.location.href = "/projects/";
           },
         },{id: "nav-publications",
-          title: "Publications",
+          title: "publications",
           description: "My publications.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
           },
         },{id: "nav-repositories",
-          title: "Repositories",
+          title: "repositories",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
           section: "Navigation",
           handler: () => {
