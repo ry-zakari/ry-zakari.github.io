671d37d054e59689eedfde5c903ab426405512e7
ry-zakari <rufaig6@gmail.com>
2025-07-04 18:30:51 +0000

    Deploying to gh-pages from @ ry-zakari/ry-zakari.github.io@7c25937c68d5235e3f81d811ffc9514da18722f4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,7 +17,7 @@ ninja.data = [{
             window.location.href = "/projects/";
           },
         },{id: "nav-publications",
-          title: "publications",
+          title: "Publications",
           description: "My publications.",
           section: "Navigation",
           handler: () => {
