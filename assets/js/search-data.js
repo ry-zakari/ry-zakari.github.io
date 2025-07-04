dc4f0d2b4c1794762f5f99418d3de5d0c4bf4eb9
ry-zakari <rufaig6@gmail.com>
2025-07-04 17:15:24 +0000

    Deploying to gh-pages from @ ry-zakari/ry-zakari.github.io@b114c960fef2a886fde07e2916371e5ca3b20229 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -421,8 +421,8 @@ ninja.data = [{
             window.location.href = "/blog/2015/formatting-and-links/";
           
         },
-      },{id: "books-",
-          title: '',
+      },{id: "books-the-four-agreements",
+          title: 'The Four Agreements',
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
